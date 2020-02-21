462c98b8da3d8a8b0b162845dc3e9d9874f3b81a
Caleb Lim <caleb.lim@mail.mcgill.ca>
2020-02-21 13:10:13 -0500

    Light localizing at waypoint in development

--- src/ca/mcgill/ecse211/project/LightLocalizer.java
+++ src/ca/mcgill/ecse211/project/LightLocalizer.java
@@ -3,8 +3,11 @@ package ca.mcgill.ecse211.project;
 import static ca.mcgill.ecse211.project.Main.sleepFor;
 //static import to avoid duplicating variables and make the code easier to read
 import static ca.mcgill.ecse211.project.Resources.*;
+import static ca.mcgill.ecse211.project.Driver.*;
+import static ca.mcgill.ecse211.project.Main.*;
 
 import lejos.robotics.SampleProvider;
+import lejos.hardware.Sound;
 
 public class LightLocalizer {
   
@@ -12,10 +15,13 @@ public class LightLocalizer {
   
       public static float colourIDLeft;
       public static float colourIDRight;
+      public static float colourIDFront;
       static SampleProvider colourValueLeft = LEFT_COL_SENSOR.getMode("Red"); 
       static SampleProvider colourValueRight = RIGHT_COL_SENSOR.getMode("Red"); 
+      static SampleProvider colourValueFront = FRONT_COL_SENSOR.getMode("Red"); 
       static float[] colourDataLeft = new float[colourValueLeft.sampleSize()];
       static float[] colourDataRight = new float[colourValueRight.sampleSize()];
+      static float[] colourDataFront = new float[colourValueFront.sampleSize()];
     
       /**
        * Get the colorID from the left color sensor.
@@ -27,10 +33,18 @@ public class LightLocalizer {
       
       /**
        * Get the colorID from the right color sensor.
-       * @return colorIDLeft  variable
+       * @return colorIDRight  variable
        */
       public static float getColourIDRight() {
         return colourIDRight;
+      }
+      
+      /**
+       * Get the colorID from the front color sensor.
+       * @return colorIDFront  variable
+       */
+      public static float getColourIDFront() {
+        return colourIDFront;
       }
     
       /**
@@ -60,6 +74,86 @@ public class LightLocalizer {
             }
           }
         }).start();
+      }
+      
+      /**
+       * This method will read data from the top light sensor
+       */
+      public static double readFrontLightData() {
+        FRONT_COL_SENSOR.getMode("Red").fetchSample(colourDataFront, 0);
+        colourIDFront = colourDataFront[0]*100;
+        return colourIDFront;
+      }
+      
+      /**
+       * Localizing method using the light sensor to get closer to a specified grid position.
+       * 
+       * @param x xPosition to localize to in grid coordinates
+       * @param y yPosition to localize to in grid coordinates
+       */
+      public static void lightLocToPoint(int x, int y) {
+        //double currentAngle = odo.getXyt()[2];
+        double lineAngles[] = new double[4];
+        int counter = 0;
+        
+        topMotor.setSpeed(40);
+        topMotor.rotate(110);
+        
+        //rotate 360 degrees to detect black lines
+        rotate();
+        
+        //store thetas when black lines are read from light sensor
+        while (counter != 4) {
+          while (readFrontLightData() > BLACK_LINE_THRESHOLD) {
+          }
+
+          lineAngles[counter] = odo.getXyt()[2]; // Record the angle at which we detected the line.
+          
+          counter++;
+
+          sleepThread(1f); // wait for a second to avoid multiple detections of the same line.
+      }
+        
+        // calculate the x and y position relative to the grid point that we inputted
+        double xPos = -SENSOR_TO_CENTER * Math.cos((lineAngles[2] - lineAngles[0]) / 2);
+        double yPos = -SENSOR_TO_CENTER * Math.cos((lineAngles[3] - lineAngles[1]) / 2);
+        double dTheta = -0.5 * Math.abs(lineAngles[1] - lineAngles[3]) + Math.PI - lineAngles[3];
+        
+        //odo.setTheta(odo.getXyt()[2] + dTheta);
+
+        //correct the y position similar to when we localized to (1,1)
+        if (yPos < 0) {
+          Navigation.turnTo(0);
+          Driver.moveStraightFor(-2*LIGHT_TO_CENTER);
+          localizeToOneOne();
+          Driver.moveStraightFor(LIGHT_TO_CENTER);
+        } else if (yPos > 0) {
+          Navigation.turnTo(180);
+          Driver.moveStraightFor(-2*LIGHT_TO_CENTER);
+          localizeToOneOne();
+          Driver.moveStraightFor(LIGHT_TO_CENTER);
+        }
+        
+        //correct the x position similar to when we localized to (1,1)
+        if (xPos < 0) {
+          Navigation.turnTo(90);
+          Driver.moveStraightFor(-2*LIGHT_TO_CENTER);
+          localizeToOneOne();
+          Driver.moveStraightFor(LIGHT_TO_CENTER);
+          odo.setTheta(90);
+        } else if (xPos > 0) {
+          Navigation.turnTo(270);
+          Driver.moveStraightFor(-2*LIGHT_TO_CENTER);
+          localizeToOneOne();
+          Driver.moveStraightFor(LIGHT_TO_CENTER);
+          odo.setTheta(270);
+        }
+        
+        //correct odometer values
+        odo.setX(x*TILE_SIZE);
+        odo.setY(y*TILE_SIZE);
+        
+        topMotor.rotate(-110);
       }
     
       /**
@@ -92,4 +186,11 @@ public class LightLocalizer {
 //          }
 //        }).start();
 //      }
+      
+      private static void sleepThread(float seconds) {
+        try {
+            Thread.sleep((long) (seconds * 1000));
+        } catch (Exception e) {
+        }
+    }
 }
