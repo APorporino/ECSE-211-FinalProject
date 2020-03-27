adad3dd028911edc167bbcf1b5d2767d224c5413
Anthony Porporino <anthony.porporino21@gmail.com>
2020-03-27 18:54:36 -0400

    Addition of Tow, WiFi and ObstacleAvoidance classes

---
+++ src/ca/mcgill/ecse211/project/ObstacleAvoidance.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse211.project;
+/**
+ * This class will be used to control the ultrasonic sensor.
+ * @author Team06
+ *
+ */
+public class ObstacleAvoidance implements Runnable{
+
+  @Override
+  /**
+   * This method will run the thread to constantly avoid obstacles.
+   */
+  public void run() {
+    
+    
+  }
+  
+  /**
+   * Constructor
+   */
+  public ObstacleAvoidance() {
+    
+  }
+  
+
+}
---
+++ src/ca/mcgill/ecse211/project/Tow.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse211.project;
+
+/**
+ * This class will control the mechanism used to attach to the stranded vehicle.
+ * @author Team06
+ *
+ */
+public class Tow {
+  
+  /**
+   * This method will attach our robot to the stranded vehicle
+   */
+  public static void attachToVehicle() {
+    
+  }
+
+}
